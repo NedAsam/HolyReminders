970059ff919ef0947ee0414347ab0cd24fe96083
NedAsam <ned.asam@frontier.com>
2022-07-11 21:31:01 -0400

    added "evenings" page.....

---
+++ dataModules/data02.js
@@ -0,0 +1,25 @@
+/* Data for "circles" page -- first commit*/
+
+/* import {locationArray} from "./addrData"; */
+
+console.log("here is a test");
+
+/* Banner on home page - 00 */
+const heading02 = [
+    `AND HE SAID`,
+    `TO HIM,`,
+    `"FOLLOW ME" <span class="footNoteRef">[2]</span>`
+ ]
+
+
+
+/* foot notes */
+const footNotes = [
+    `[1] Raphael, "The Transfiguration" -- Public domain, via Wikimedia Commons`,
+
+    `[2] another reference`
+   
+]
+
+
+export {heading02, footNotes};
---
+++ modules/eveningDisplay.js
@@ -0,0 +1,26 @@
+console.log ("Hello World from the circle module");
+
+import  {heading02, footNotes} from "../dataModules/data02.js";
+import  {DisplayLi, DisplayParaList, DisplayStr } from "./classDisplay.js";
+
+console.log(heading02)
+
+
+
+
+const displayBannerList = new DisplayParaList ('banner-paraList', heading02);
+displayBannerList.displayList();
+
+/*
+const displayCont00 = new DisplayStr ('cont01', content01);
+displayCont00.displayString();
+
+const introContent = new DisplayLi('intro-list', introduction);
+introContent.displayList();
+
+const introSubContent = new DisplayLi('intro-subList', introSub);
+introSubContent.displayList();
+*/
+
+const displayNotes = new DisplayLi( 'footNote02', footNotes);
+displayNotes.displayList();
