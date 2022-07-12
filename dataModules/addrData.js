4b57e3e5974339019ad2f99c3516c2e9dfe25e5b
NedAsam <60985708+NedAsam@users.noreply.github.com>
2022-07-12 17:35:23 -0400

    Update addrData.js

--- dataModules/addrData.js
+++ dataModules/addrData.js
@@ -1,4 +1,4 @@
-/* Data for "circles" page -- first commit*/
+/* Addres data...-- first commit*/
 
 class Location  {
     Constructor(id, name, addr){
@@ -13,11 +13,13 @@ const addr0 = "15 Indian Spring Lane, Rochester, NY 14618";
 const addr1 = "155 Golf Ave. Pittsford, NY 14534";
 const addr2 = "127 Chapel Drive, Syracuse, NY 13291";
 const addr3 = "one Randall Street, Pembroke, MA 02359";
+const addr4 = "TBD"; 
 
 const loc00 = new Location(0, 'Principe Home', addr0);
 const loc01 = new Location(1, 'Monroe Country Club', addr1);
 const loc02 = new Location(2, 'Holy Family Church', addr2);
 const loc03 = new Location(3, 'Arnold Hall', addr3);
+const loc04 = new Location(4, 'To be announced', addr4);
 
 
 const locationArray = [loc00, loc01, loc02, loc03];
