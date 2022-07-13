c47497d2e3adf253472216f5a4800fe8abf306af
NedAsam <ned.asam@frontier.com>
2022-07-13 18:07:43 -0400

    word smithing content

--- dataModules/data00.js
+++ dataModules/data00.js
@@ -8,18 +8,18 @@ const content00 = `"You and I belong to Christ’s family, for 'he himself has c
 const introduction = [
     `Welcome and thank you for your interest in Opus Dei here in Western NY.`,
     `We are all pilgrims on our earthly journey toward eternity. In this life, we all have our crosses -- anxieties, fears, illnesses, disappointments, regrets, et. al. In our hearts, we all desire to be able to echo St. Paul in declaring ...`,
-    `"I have fought the good fight, I have finished the race, I have kept the faith. From now on there is reserved for me the crown of righteousness, which the Lord, the righteous judge, will give me on that Day, and not only to me but also to all who have loved his appearing."<span class="footNoteRef">[3]</span>`,
-    `However, "fighting the good fight" can seem rather daunting to most of us, much of the time. We read the lives of the saints, but we experience the pressures of every day life. We can be tempted to think that being "saintly" is only for those who live in convents or monastaries.`,
-    `St. Josemaria Escriva was clearly aware of this flase mindset. Early on  in his priestly ministry he became convinced that God wanted him to help everyone to realize that they were all called by God to live saintly lives amidst their everyday activities. This led St. Josemaria Escriva to form what became know as "Opus Dei" -- i.e. "The Work of God."`,
-    `To get to know St. Josmaria Escriva and  to learn about Opus Dei, begin  by frequenting the <a href="https://OpusDei.org">Opus Dei website.</a>`,
-    `Further, this introductory website is a resource for keeping keeping track of the local Opus Dei activities (follow navigation links).`
+    `<span class="quoteFormat">I have fought the good fight, I have finished the race, I have kept the faith. From now on there is reserved for me the crown of righteousness, which the Lord, the righteous judge, will give me on that Day, and not only to me but also to all who have loved his appearing."</span><span class="footNoteRef">[3]</span>`,
+    `However, "fighting the good fight" can seem rather daunting to most of us, much of the time. We may read the lives of the saints, but, every day, we experience the pressures of every day life. We can be tempted to think that being "saintly" is only for those who live in convents or monastaries.`,
+    `St. Josemaria Escriva was clearly aware of this false mindset. Early on  in his priestly ministry he became convinced that God wanted him to help everyone to realize that they are all called by God to live saintly lives amidst our everyday activities. This led St. Josemaria Escriva to form what became know as "Opus Dei" -- i.e. "The Work of God."`,
+    `To get to know St. Josmaria Escriva and to learn about Opus Dei, begin  by frequenting the <a href="https://OpusDei.org">Opus Dei website</a>. This beautiful website comprises biographical information and videos of St. Josemaria Escriva, background information about Opus Dei, continuously updated teachings, and many personal witnesses form Opus Dei members and supporters.`,
+    `Further, this introductory website is a resource for keeping keeping track of the local Opus Dei activities(follow navigation links -- embedded below or in the menu at the top of pages).`
 
 ]
 
 const introSub = [
     ` - small group gatherings called "<a href="./pages/circles.html">circles</a>" or classes.`,
     ` - <a href="./pages/evenings.html">evenings</a> of recollection comprising adoration, confession, meditations, and teachings.`,
-    ` - annual retreats, conferences, <a href="./pages/events.html">events</a>.`
+    ` - annual retreats, conferences, and other <a href="./pages/events.html">events</a>.`
 ]
 
 
