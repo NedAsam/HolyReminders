5e2effeb407ff3b704d0618fac39acc0a344c952
NedAsam <ned.asam@frontier.com>
2022-07-29 13:41:52 -0400

    updated 'evening' formats

--- dataModules/data02.js
+++ dataModules/data02.js
@@ -6,6 +6,26 @@ console.log("here is a test");
 
 /* Banner on home page - 00 */
 const heading00 = [
+    `<br>`,
+    `<br>`,
+    `<br>`,
+    `THIS IS MY SON,`,
+    `MY CHOSEN;`,
+    `LISTEN TO HIM`,
+    `<span class="imgRef">Luke 9:35 [2]`, 
+    `<br>`,
+    `<br>`,
+    `<br>`, 
+    `<br>`, 
+    `<br>`, 
+    `<br>`,
+    `...Jesus rebuked the unclean spirit,`,
+    `healed the boy, and`,
+    `and gave him back to his father.`,
+    `<span class="imgRef">Luke 9:42 [3]`
+ ]
+
+ const heading00a = [
     `THIS IS MY SON,`,
     `MY CHOSEN;`,
     `LISTEN TO HIM <span class="footNoteRef">[2]</span>`  
@@ -14,9 +34,10 @@ const heading00 = [
 /* foot notes */
 const footNotes = [
     `[1] Raphael, "The Transfiguration" -- Public domain, via Wikimedia Commons`,
-    `[2] Luke 9:35, Word on Fire Bible, Vol 1 -- Public domain, via Wikimedia Commons`
+    `[2] Luke 9:35, Word on Fire Bible, Vol 1`,
+    `[3] Luke 9:42, Word on Fire Bible, Vol 1`
    
 ]
 
 
-export {heading02, footNotes};
+export {heading00, heading00a, footNotes};
--- modules/eveningDisplay.js
+++ modules/eveningDisplay.js
@@ -1,15 +1,17 @@
 console.log ("Hello World from the evening module");
 
-import  {heading02, footNotes} from "../dataModules/data02.js";
+import  {heading00, heading00a, footNotes} from "../dataModules/data02.js";
 import  {DisplayList, DisplayStr } from "./classDisplay.js";
 
-console.log(heading02)
+console.log(heading00);
+console.log(heading00a);
 
-
-
-
-const displayBannerList = new DisplayList ('banner-paraList', heading00, 'h2');
+const displayBannerList = new DisplayList ('content00', heading00, 'p');
 displayBannerList.displayList();
+
+const displayBannerLista = new DisplayList ('content00a', heading00a, 'p');
+displayBannerLista.displayList();
+
 
 /*
 const displayCont00 = new DisplayStr ('cont01', content01);
