c84fa2df0084c06b54911d0f88da5c9ab0ed1c89
NedAsam <ned.asam@frontier.com>
2022-07-29 15:34:05 -0400

    updated 'evening' presentation

--- dataModules/data02.js
+++ dataModules/data02.js
@@ -31,6 +31,38 @@ const heading00 = [
     `LISTEN TO HIM <span class="footNoteRef">[2]</span>`  
  ]
 
+ const content02t = [
+    `Syracuse, NY -- Evening of recollection -- 6:45 pm`,
+    
+ ]
+ const content02i = [
+    `(Third Friday of selected months.)`,
+    `<br>`,
+    `Holy Family Church -- 127 Chapel Dr, Syracuse, NY 13219`,
+    `Opus Dei priest available for confession / spiritual guidance from about 5:30 pm`,
+    `<br>`,
+    ` <b>Scheduled Dates:</b>`
+ ]
+const content02d1 = [
+     `8/19/2022`, `9/16/2022`, `11/18/2022`
+]
+const content02d2 = [
+     `12/16/2022`, `2/17/2023`, `3/17/2023`
+]
+const content03t = [
+    `Rochester, NY -- Evening of recollection -- 6:45 pm`,
+ ]
+const content03i = [
+    `(Third Friday of selected months.)`,
+    `<br>`,
+    `St. Jerome's Church -- 207 Garfield St., East Rochester, New York 14445`,
+    `Opus Dei priest available for confession / spiritual guidance  from about 5:30 pm`,
+    `<br>`,
+    ` <b>Scheduled Dates:</b>`
+ ]
+const content03d1 = [
+     `10/21/2022`, `1/20/2023`, `4/21/2023`, `7/21/2023`
+]
 /* foot notes */
 const footNotes = [
     `[1] Raphael, "The Transfiguration" -- Public domain, via Wikimedia Commons`,
@@ -40,4 +72,7 @@ const footNotes = [
 ]
 
 
-export {heading00, heading00a, footNotes};
+export {heading00, heading00a, 
+        content02t, content02i, content02d1, content02d2, 
+        content03t, content03i, content03d1,  
+        footNotes};
--- modules/eveningDisplay.js
+++ modules/eveningDisplay.js
@@ -1,6 +1,9 @@
 console.log ("Hello World from the evening module");
 
-import  {heading00, heading00a, footNotes} from "../dataModules/data02.js";
+import  {heading00, heading00a, 
+        content02t, content02i, content02d1, content02d2, 
+        content03t, content03i, content03d1,  
+        footNotes} from "../dataModules/data02.js";
 import  {DisplayList, DisplayStr } from "./classDisplay.js";
 
 console.log(heading00);
@@ -11,6 +14,34 @@ displayBannerList.displayList();
 
 const displayBannerLista = new DisplayList ('content00a', heading00a, 'p');
 displayBannerLista.displayList();
+
+
+const displayContent02t = new DisplayList ('content02t', content02t, 'p');
+displayContent02t.displayList();
+
+const displayContent02i = new DisplayList ('content02i', content02i, 'p');
+displayContent02i.displayList();
+
+const displayContent02d1 = new DisplayList ('content02d1', content02d1, 'li');
+displayContent02d1.displayList();
+
+const displayContent02d2 = new DisplayList ('content02d2', content02d2, 'li');
+displayContent02d2.displayList();
+
+
+
+const displayContent03t = new DisplayList ('content03t', content03t, 'p');
+displayContent03t.displayList();
+
+const displayContent03i = new DisplayList ('content03i', content03i, 'p');
+displayContent03i.displayList();
+
+const displayContent03d1 = new DisplayList ('content03d1', content03d1, 'li');
+displayContent03d1.displayList();
+
+
+
+
 
 
 /*
