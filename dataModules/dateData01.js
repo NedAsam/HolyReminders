d1f615059cfe7be000393c1c78264d25351a4f85
NedAsam <Ned.Asam@Frontier.com>
2023-03-13 09:53:14 -0400

    Updated dates 20230313

--- dataModules/dateData01.js
+++ dataModules/dateData01.js
@@ -2,7 +2,7 @@
 /* Dates for morning circles at Monroe Country Club */
 
 const content02d1 = [
-    `3/9/2023`, `4/6/2023`, `5/4/2023`, `6/1/2023`
+    `3/9/2023`, `4/13/2023`, `5/4/2023`, `6/1/2023`
 ]
 const content02d2 = [
     `7/6/2023`, `8/3/2023`, `9/7/2023`, `10/5/2023`
@@ -11,7 +11,7 @@ const content02d2 = [
 /* Dates for evening circles at David Principe's home */
 
 const content03d1 = [
-    `3/4/2023`, `4/7/2023`, `5/5/2023`, `6/2/2023`
+    `3/4/2023`, `4/14/2023`, `5/5/2023`, `6/2/2023`
 ]
 const content03d2 = [
     `7/7/2023`, `8/4/2023`, `9/1/2023`, `10/6/2023`
